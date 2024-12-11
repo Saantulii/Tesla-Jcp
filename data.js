373f346dc77f3b3f1b394a7c0e599a9977d6733f
Saantulii <santiagomunoz785@gmail.com>
2024-12-10 22:07:44 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const pagesData = {
-    "index.php": [
+    "index.html": [
         "¡Bienvenidos a Tesla José C. Paz! Nos complace recibirte en nuestra comunidad educativa.",
         "En nuestra institución, creemos en la formación integral de nuestros estudiantes.",
         "Fomentamos la curiosidad intelectual y el pensamiento crítico.",
@@ -7,39 +7,39 @@ const pagesData = {
         "Ubicación: José C. Paz, Buenos Aires",
     ],
 
-    "1ro 1ra.php": ["1ro 1ra", "Horario de 1ro 1ra"],
-    "1ro 2da.php": ["1ro 2da"],
-    "1ro 3ra.php": ["1ro 3ra"],
-    "1ro 4ta.php": ["1ro 4ta"],
-    "2do 1ra.php": ["2do 1ra"],
-    "2do 2da.php": ["2do 2da"],
-    "2do 3ra.php": ["2do 3ra"],
-    "2do 4ta.php": ["2do 4ta"],
-    "3ro 1ra.php": ["3ro 1ra"],
-    "3ro 2da.php": ["3ro 2da"],
-    "3ro 3ra.php": ["3ro 3ra"],
-    "3ro 4ta.php": ["3ro 4ta"],
-    "4to 1ra.php": ["4to 1ra"],
-    "4to 2da.php": ["4to 2da"],
-    "4to 3ra.php": ["4to 3ra"],
-    "4to 4ta.php": ["4to 4ta"],
-    "4to 5ta.php": ["4to 5ta"],
-    "5to 1ra.php": ["5to 1ra"],
-    "5to 2da.php": ["5to 2da"],
-    "5to 3ra.php": ["5to 3ra"],
-    "6to 1ra.php": ["6to 1ra"],
-    "6to 2da.php": ["6to 2da"],
-    "6to 3ra.php": ["6to 3ra"],
-    "7mo 1ra.php": ["7mo 1ra"],
-    "7mo 2da.php": ["7mo 2da"],
-    "7mo 3ra.php": ["7mo 3ra"],
+    "1ro 1ra.html": ["1ro 1ra", "Horario de 1ro 1ra"],
+    "1ro 2da.html": ["1ro 2da"],
+    "1ro 3ra.html": ["1ro 3ra"],
+    "1ro 4ta.html": ["1ro 4ta"],
+    "2do 1ra.html": ["2do 1ra"],
+    "2do 2da.html": ["2do 2da"],
+    "2do 3ra.html": ["2do 3ra"],
+    "2do 4ta.html": ["2do 4ta"],
+    "3ro 1ra.html": ["3ro 1ra"],
+    "3ro 2da.html": ["3ro 2da"],
+    "3ro 3ra.html": ["3ro 3ra"],
+    "3ro 4ta.html": ["3ro 4ta"],
+    "4to 1ra.html": ["4to 1ra"],
+    "4to 2da.html": ["4to 2da"],
+    "4to 3ra.html": ["4to 3ra"],
+    "4to 4ta.html": ["4to 4ta"],
+    "4to 5ta.html": ["4to 5ta"],
+    "5to 1ra.html": ["5to 1ra"],
+    "5to 2da.html": ["5to 2da"],
+    "5to 3ra.html": ["5to 3ra"],
+    "6to 1ra.html": ["6to 1ra"],
+    "6to 2da.html": ["6to 2da"],
+    "6to 3ra.html": ["6to 3ra"],
+    "7mo 1ra.html": ["7mo 1ra"],
+    "7mo 2da.html": ["7mo 2da"],
+    "7mo 3ra.html": ["7mo 3ra"],
 
-    "novedades.php": [
+    "novedades.html": [
         "Últimas noticias",
         "Eventos recientes",
         "Nuevas propuestas educativas."
     ],
-    "nosotros.php": [
+    "nosotros.html": [
         "Sobre Nosotros",
         "Tesla José C. Paz tiene una historia de excelencia educativa.",
         "Nuestro equipo docente está comprometido con el desarrollo personal y profesional de los estudiantes.",
@@ -57,7 +57,7 @@ const pagesData = {
         "Logros y Reconocimientos",
     ],
 
-    "calendario.php": [
+    "calendario.html": [
         "Calendario",
         "Feriado",
         "Días festivos del Colegio",
@@ -76,29 +76,29 @@ const pagesData = {
         "Diciembre"
     ],
 
-    "cd.php": [
+    "cd.html": [
         "Centro de estudiantes",
         "Nuestras Propuestas",
         "Próximos Eventos",
         "presidentes",
     ],
     
-    "login.php": [
+    "login.html": [
         "Iniciar sesión",
         "Registrarse",
         "login",
     ],
 
-    "previas.php": [
+    "previas.html": [
         "Previas",
     ],
 
-    "equipo.php": [
+    "equipo.html": [
         "directivo",
         "directivos",
     ],
 
-    "nikolatesla.php": [
+    "nikolatesla.html": [
         "nikola tesla",
         "nicola tesla",
         "mi cola apesta"
